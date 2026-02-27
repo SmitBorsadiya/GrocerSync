86e2d4dfd4f3ba04d7afe80a29cf0f8447a131ed
Smit <borsadiya5525smit@gmail.com>
2026-02-27 22:35:26 +0530

    Initial commit

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'sidebar' => false,
+])
+
+@if($sidebar)
+    <flux:sidebar.brand name="GrocerSync" {{ $attributes }}>
+        <x-slot name="logo" class="flex aspect-square size-9 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+            <img src="/logo.svg" alt="Logo" class="size-8 rounded-md">
+        </x-slot>
+    </flux:sidebar.brand>
+@else
+    <flux:brand name="GrocerSync" {{ $attributes }}>
+        <x-slot name="logo" class="flex aspect-square size-9 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+            <img src="/logo.svg" alt="Logo" class="size-8 rounded-md">
+        </x-slot>
+    </flux:brand>
+@endif
